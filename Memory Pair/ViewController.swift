8db4215fb67259331b8489542a5f4538c0dc75ac
simerpreetjasssal <s.jassal599@mybvc.ca>
2020-07-12 21:03:03 -0600

    Initial Commit

---
+++ Memory Pair/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memory Pair
+//
+//  Created by Simerpreet singh Jassal on 2020-07-12.
+//  Copyright © 2020 Simerpreet singh Jassal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
